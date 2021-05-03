5c1e33f41f2e03fc16e40d8a5a27a76915cc91cf
Stacy Read <stacy.read.007@gmail.com>
2021-05-03 16:12:14 -0500

    Changed var to let and click to on

--- pq.js
+++ pq.js
@@ -1,48 +1,47 @@
 $(document).ready(function () {
-    // A constant for drawing the status bars
-    const PIXELS_PER_UNIT = 10;
-
-    // The hero object is global
-    var hero = {};
-
-    // An array of potion objects
-    var potionArray = [
-        {image: "img/blue1.png", name: "Sparkling Blue", stat: "magic", mod: 5}
-    ];
-
-    // Needs to be global
-    var currentPotion;
-
     // Add the click listener to the image
-    $("#chest").click(openChest);
+    $("#chest").on("click", openChest);
 
     // Add click listener to the "Drink" button
-    $("#drinkButton").click(drinkPotion);
+    $("#drinkButton").on("click", drinkPotion);
 
     // Add click listener to the "Save" button
-    $("#saveButton").click(savePotion);
+    $("#saveButton").on("click", savePotion);
 
     // Draw the initial status bars
     drawHero();
-
-    // Clicking on the image opens the chest to reveal a potion
-    function openChest() {
-
-    }
-
-    // Add the potion to the hero's backpack
-    function savePotion() {
-
-    }
-
-    // Adjust's the hero's stats according to the potion's properties
-    function drinkPotion() {
-
-    }
-
-    // Draw the hero's stat bars and backpack contents
-    function drawHero() {
-
-    }
-
 });
+
+// A constant for drawing the status bars
+const PIXELS_PER_UNIT = 10;
+
+// The hero object is global
+let hero = {};
+
+// An array of potion objects
+let potionArray = [
+    {image: "img/blue1.png", name: "Sparkling Blue", stat: "magic", mod: 5}
+];
+
+// Needs to be global
+let currentPotion;
+
+// Clicking on the image opens the chest to reveal a potion
+function openChest() {
+
+}
+
+// Add the potion to the hero's backpack
+function savePotion() {
+
+}
+
+// Adjust's the hero's stats according to the potion's properties
+function drinkPotion() {
+
+}
+
+// Draw the hero's stat bars and backpack contents
+function drawHero() {
+
+}
